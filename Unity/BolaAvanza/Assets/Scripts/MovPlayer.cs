8a4aeb3cbbcbf24e2cba1e520988e993d2beacfb
mauroditDev <maurodit.dev@gmail.com>
2017-01-25 10:26:33 +0100

    iniciando Bola

---
+++ Unity/BolaAvanza/Assets/Scripts/MovPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovPlayer : MonoBehaviour {
+
+    public float velocidadBola;
+    private Vector3 dir;
+
+	void Start () {
+        dir = new Vector3(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (dir == new Vector3(-1, 0))
+            {
+                dir = new Vector3(0, 0, 1);
+            }
+            else
+            {
+                dir = new Vector3(-1, 0);
+            }
+        }
+
+        this.transform.position += velocidadBola * dir * Time.deltaTime;
+
+        if(this.transform.position.y < -3f)
+        {
+           // Debug.Log("Eres un maldito perdedor");
+        }
+
+	}
+}
---
+++ Unity/BolaAvanza/Assets/TileScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour {
+
+    //public TileManager tileManager;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        //tileManager.newTile()
+    }
+
+}
