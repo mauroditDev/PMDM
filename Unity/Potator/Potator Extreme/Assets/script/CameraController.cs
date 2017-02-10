5b208fc0865cf2557bd2ecde34e67dd74a8318b3
mauroditDev <maurodit.dev@gmail.com>
2017-02-10 12:26:14 +0100

    añadiendo fondos

---
+++ Unity/Potator/Potator Extreme/Assets/script/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = player.transform.position - this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = player.transform.position + offset;
+	}
+}
--- Unity/Potator/Potator Extreme/Assets/script/PlayerCont.cs
+++ Unity/Potator/Potator Extreme/Assets/script/PlayerCont.cs
@@ -7,17 +7,34 @@ public class PlayerCont : MonoBehaviour {
     private SpriteRenderer spr;
     private bool derecha;
     public GameObject gameManager;
+    public bool suelo;
+    public float velocidad;
+    public float salto;
+    private Rigidbody rb;
+    public float tolerancia;
+    public float lastY;
 
 	// Use this for initialization
 	void Start () {
         derecha = true;
         animator = this.gameObject.GetComponent<Animator>();
         spr = this.gameObject.GetComponent<SpriteRenderer>();
+        suelo = true;
+        rb = this.gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        Debug.Log(this.transform.position.y);
+        if(this.transform.position.y-lastY > (tolerancia*(-1)) && this.transform.position.y - lastY < tolerancia)
+        {
+            suelo = true;
+        }
+        else
+        {
+            suelo = false;
+        }
+
         if (Input.GetAxis("Horizontal") != 0)
         {
             animator.Play("WalkRt");
@@ -49,7 +66,17 @@ public class PlayerCont : MonoBehaviour {
                 {
                     if (Input.GetAxis("Vertical") > 0)
                     {
-                        animator.Play("Jump");
+                        if (suelo)
+                        {
+                            rb.AddForce(new Vector3(0,1) * salto);
+                            animator.Play("Jump");
+                            suelo = false;
+                        }
+                        else
+                        {
+                            Debug.Log("no saltes");
+                        }
+                        
                     }
                     else
                     {
@@ -58,8 +85,12 @@ public class PlayerCont : MonoBehaviour {
                 }
             }
         }
-        
-        
+
+        if (suelo)
+        {
+            rb.AddForce(new Vector3(1, 1) * Input.GetAxis("Horizontal")* velocidad * Time.deltaTime);
+        }
+        lastY = this.transform.position.y;
 	}
 
     
